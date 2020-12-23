8d71deca19683554d333fcb2f888e808e8025d8f
Vitorhugo17 <martinsvitor11@gmail.com>
2020-12-23 13:00:51 +0000

    alt

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
-from flask import Flask
-from flask import render_template
+import warnings
+import flask
+from flask import request, Response, jsonify
+import json
+
+warnings.filterwarnings('ignore')
 
 # creates a Flask application, named app
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 # a route where we will display a welcome message via an HTML template
-@app.route("/")
-def hello():
-    message = "Hello, World"
-    return render_template('index.html', message=message)
+@app.route("/", methods=['GET'])
+def home():
+    resultado = {'idUtilizador': 2}
+    return Response(json.dumps(resultado),  mimetype='application/json')
 
-app.run(debug=True)
+app.run()
