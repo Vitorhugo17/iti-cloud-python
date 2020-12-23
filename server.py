8667056a55be1636b280b66d7f95ef0c10f11287
Vitorhugo17 <martinsvitor11@gmail.com>
2020-12-23 12:49:28 +0000

    alt

--- server.py
+++ server.py
@@ -10,6 +10,4 @@ def hello():
     message = "Hello, World"
     return render_template('index.html', message=message)
 
-# run the application
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
